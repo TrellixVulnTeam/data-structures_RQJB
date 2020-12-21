3b12e5ff5d8402a2d088772fb783da6ea2fffb4b
Tayfun Kuşçu <69037566+tayfunkscu@users.noreply.github.com>
2020-12-21 11:55:11 +0300

    new files

--- Heaps/C++/Heaps/main.cpp
+++ Heaps/C++/Heaps/main.cpp
@@ -75,11 +75,50 @@ void MinHeapImplementedWithPriorityQueue(){
         pq.pop();
     }
 }
+void display(const vector<int>& vec){
+    for(int num: vec)
+        cout<<num<<" ";
+    cout<<endl;
+}
+void HeapInSTL(){
+    vector<int> vec{7,6,8,5,9,4,10,2,3};
+    //Max Heap
+    make_heap(vec.begin(), vec.end());  //Vektörü heap haline getirir
+    cout<<"Heap:";
+    display(vec);
+    cout<<"Max: "<<vec.front()<<endl;
+
+    vec.push_back(30);
+    push_heap(vec.begin(), vec.end());  //Vektöre yeni elemean eklenmesi durumda yeniden düzenlemek için kullanılır
+    cout<<"Push heap:";
+    display(vec);
+    cout<<"Max: "<<vec.front()<<endl;
+
+    pop_heap(vec.begin(), vec.end()); //Maksimum değerli elemanı silmek için kullanılır
+    vec.pop_back();
+    cout<<"Pop heap:";
+    display(vec);
+    cout<<"Max: "<<vec.front()<<endl;
+
+    sort_heap(vec.begin(), vec.end());
+    cout<<"Sort heap:";
+    display(vec);
+
+    cout<<"Is Heap:"<<is_heap(vec.begin(), vec.end())<<endl;
+    make_heap(vec.begin(), vec.end());
+    cout<<"Is Heap:"<<is_heap(vec.begin(), vec.end())<<endl;
+
+    auto it = is_heap_until(vec.begin(), vec.end());
+    cout<<"Heap:";
+    for (auto i = vec.begin(); i!=it; i++)
+        cout<<*i<<" ";
+}
 int main() {
     //test1();
     //test2();
-    test3();
+    //test3();
     //MaxHeapImplementedWithPriorityQueue();
     //MinHeapImplementedWithPriorityQueue();
+    HeapInSTL();
     return 0;
 }
